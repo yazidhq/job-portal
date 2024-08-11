a6fc2d0ccaab0f5d2eeafbd14997169889c5d1dd
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-08-11 16:58:14 +0700

    create: request validations

---
+++ app/Http/Requests/UpdateCompanyJobRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompanyJobRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'salary' => ['required', 'integer'],
+            'company_id' => ['required', 'integer'],
+            'category_id' => ['required', 'integer'],
+            'name' => ['required', 'string', 'max:255'],
+            'skill_level' => ['required', 'string', 'max:255'],
+            'location' => ['required', 'string', 'max:255'],
+            'type' => ['required', 'string', 'max:255'],
+            'thumbnail' => ['sometimes', 'image', 'mimes:png, jpg, jpeg'],
+            'responsibilities .* ' => 'required|string |max:255',
+            'qualifications .* ' => 'required|string|max:255',
+            'about' => ['required', 'string', 'max:65535'],
+        ];
+    }
+}
